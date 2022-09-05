15c47a27f3eaf3ea67bafe022fb2aaae999b62b8
Alexis <urquizaalexis22@gmail.com>
2022-09-05 03:26:53 -0300

    created component NotFound

---
+++ components/NotFound.tsx
@@ -0,0 +1,22 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const NotFound = () => {
+  const image404 =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Pokebola-pokeball-png-0.png/800px-Pokebola-pokeball-png-0.png";
+
+  return (
+    <>
+      <div className="not-found">
+        <Typography variant="h4">
+          Sorry trainer! We couldn't find that Pokemon! Keep looking for 'em.
+        </Typography>
+      </div>
+      <div className="pokeball-container">
+        <img className="pokeball" src={image404} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
