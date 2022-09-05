9410b62fb181224d4cdb042631b1602c42eba48d
Alexis <urquizaalexis22@gmail.com>
2022-09-05 03:02:54 -0300

    added new types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,32 @@
 type Pokemon = {
-    name: string;
-    types: Types;
-    id: number;
-    front_default: string;
-    evolvesFrom: string;
-  };
-  
-  type PokemonsArray = Pokemon[];
-  
+  name: string;
+  types: Types;
+  id: number;
+  front_default: string;
+  evolvesFrom: string;
+};
+
+type PokemonsArray = Pokemon[];
+
+type Types = [
+  { slot: number; type: { name: string; url: string } },
+  { slot?: number; type?: { name?: string; url?: string } }
+];
+
+type Abilities = {
+  ability: { name: string; url: string };
+  is_hidden: boolean;
+  slot: number;
+};
+
+type SinglePokemon = {
+  abilities: Abilities[];
+  height: number;
+  weight: number;
+  front_default: string;
+  front_shiny: string;
+  id: number;
+  chain: Object;
+  types: Types;
+  name: string;
+};
