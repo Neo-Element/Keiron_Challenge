929db1126b136ccdf9566fdd14c1a9c15f81627b
Alexis <urquizaalexis22@gmail.com>
2022-09-05 22:32:14 -0300

    added back arrow

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
 import React from "react";
+import Link from "next/link";
+import { Typography, CardActionArea } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const NotFound = () => {
   const image404 =
@@ -8,8 +10,14 @@ const NotFound = () => {
   return (
     <>
       <div className="not-found">
+        <Link href={{ pathname: "/" }}>
+          <CardActionArea>
+            <ArrowBackIcon />
+          </CardActionArea>
+        </Link>
         <Typography variant="h4">
-          Sorry trainer! We couldn&apos;t find that Pokemon! Keep looking for &apos;em.
+          Sorry trainer! We couldn&apos;t find that Pokemon! Keep looking for
+          &apos;em.
         </Typography>
       </div>
       <div className="pokeball-container">
